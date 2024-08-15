1169a88769aec020395c34b8896fabc99c88664d
hamzakhan512 <hamzakh620@gmail.com>
2024-08-15 23:44:08 +0500

    add file

---
+++ even,odd.js
@@ -0,0 +1,24 @@
+let number = prompt("Please Enter The Number")
+
+if(number % 2 === 0){
+    console.log(number + "  Is Even Number. ")
+}
+else{
+    console.log(number + " Is Odd Number. ")
+}
+
+
+
+// Function to check if a number is even or odd
+// function checkEvenOrOdd(number) {
+//     if (number % 2 === 0) {
+//         console.log(number + " is an even number.");
+//     } else {
+//         console.log(number + " is an odd number.");
+//     }
+// }
+
+// // Example usage:
+// let number = prompt("Enter a number:");
+// number = parseInt(number); // Convert the input to an integer
+// checkEvenOrOdd(number);
